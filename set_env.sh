48db19db4affc40632c74d8251a8c8b602ba2dc0
joe-kwanin <kwame.kwanin@gmail.com>
2022-11-20 23:15:21 +0000

    Added the files after running the containers

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=profe55io
+export POSTGRES_HOST=database-1.cvepmgwppivl.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=udagram-11719490893-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
